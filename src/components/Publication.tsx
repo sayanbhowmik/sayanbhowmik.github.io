55bda489041c0537c128c270436ca024a9c5e4a5
anuran-Chakraborty <ch.anuraan@gmail.com>
2025-11-29 15:44:57 +0530

    Publications formatting fixed

--- src/components/Publication.tsx
+++ src/components/Publication.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
+import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 
 interface PublicationProps {
@@ -18,32 +19,63 @@ interface PublicationProps {
 const Publication = ({ publication }: PublicationProps) => {
     return (
         <>
-            <Divider
-                orientation="horizontal"
-                flexItem
-                sx={{ borderRightWidth: 2, mb: 4 }}
-            />
-            <Stack spacing={2}>
+            <Divider orientation="horizontal" flexItem sx={{ borderRightWidth: 2, mb: 4 }} />
+
+            <Stack spacing={3}>
+                {/* Title + authors row */}
                 <Typography
                     component={Link}
                     href={publication.link}
-                    sx={{
-                        textDecoration: "none",
-                        color: "inherit",
-                        "&:hover": {
-                            textDecoration: "underline",
-                        },
-                    }}
+                    sx={{ textDecoration: "none", color: "inherit", "&:hover": { textDecoration: "underline" } }}
                     variant="h5"
                 >
                     {publication.title}
                 </Typography>
 
-                <Typography variant="h6">
-                    {`${publication.authors} | DOI: ${publication.doi}`}
-                </Typography>
+                {/* Image floated left + abstract text wraps */}
+                <Box>
+                    <Box
+                        component="img"
+                        src={`/images/publications/${publication.image}`}
+                        alt="Publication"
+                        sx={{
+                            width: 300,
+                            height: "auto",
+                            borderRadius: 2,
+                            objectFit: "cover",
 
-                <Typography variant="body1">{publication.abstract}</Typography>
+                            /* FLOAT behavior:
+                             * - on md+ screens float left so paragraph wraps
+                             * - on xs screens do not float (image sits above text)
+                             */
+                            float: { xs: "none", md: "left" },
+                            mr: { xs: 0, md: 3 },
+                            mb: { xs: 2, md: 0 },
+                        }}
+                    />
+                    <Typography
+                        variant="subtitle2"
+                        sx={{
+                            fontWeight: 500,
+                            mt: 0,
+                        }}
+                    >
+                        {`${publication.authors} | DOI: ${publication.doi}`}
+                    </Typography>
+
+                    <Typography
+                        variant="body1"
+                        sx={{
+                            // ensure good readability when wrapped; allow it to flow under image naturally
+                            mt: 1,
+                            lineHeight: 1.6,
+                        }}
+                    >
+                        {publication.abstract}
+                    </Typography>
+                    {/* clear so next publication (or footer) doesn't wrap around this image */}
+                    <Box sx={{ clear: "both" }} />
+                </Box>
             </Stack>
         </>
     );

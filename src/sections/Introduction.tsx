b4d40a5a41743619c271627f448bc3a89a9af728
anuran-Chakraborty <ch.anuraan@gmail.com>
2025-11-29 16:45:11 +0530

    Add profile picture

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import Typography from "@mui/material/Typography";
 import pageContent from "@/data/pageContent.json";
+import Stack from "@mui/material/Stack";
+import CircularImage from '@/components/CircularImage';
+import Divider from "@mui/material/Divider";
 
 const Introduction = () => {
     return (
         <>
-            <Typography
-                variant="body1"
-                sx={{
-                    paddingLeft: "0.9rem",
-                    listStyleType: '"◈ "',
-                    "& li": {
-                        marginBottom: 1,
-                        listStylePosition: "outside",
-                    },
-                }}
-            >
-                {pageContent.introduction.content}
-            </Typography>
+            <Stack spacing={4} direction={"row"}>
+                <CircularImage
+                    src={`/images/${pageContent.introduction.image}`}
+                    alt="Profile Picture"
+                    size={250}
+                    border="4px solid #041220ff"
+                />
+                <Typography
+                    variant="body1"
+                    sx={{
+                        paddingLeft: "0.9rem"
+                    }}
+                >
+                    {pageContent.introduction.content}
+                </Typography>
+            </Stack>
         </>
     )
 }
